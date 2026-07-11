941a38f893497d863bb4f68dc4b169019d41a10d
JyoshnaSreepuram <sreepuramjyoshna22@gmail.com>
2026-07-11 22:28:02 +0530

    Initialize ASP.NET Core application with services

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using SmartTaskManagerAPI.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+
+// Register Task Service
+
+builder.Services.AddSingleton<TaskService>();
+
+
+
+var app = builder.Build();
+
+
+
+if(app.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+
+    app.UseSwaggerUI();
+
+}
+
+
+
+app.UseHttpsRedirection();
+
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+
+app.Run();
