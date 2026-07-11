5ff45b6072c261f7c6dbcd46cac06e13db5a1b3b
JyoshnaSreepuram <sreepuramjyoshna22@gmail.com>
2026-07-11 22:24:24 +0530

    Refactor TaskItem class into User class

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,13 @@
-namespace SmartTaskManagerAPI.Models;
-
-public class TaskItem
+namespace SmartTaskManagerAPI.Models
 {
-    public int TaskId { get; set; }
+    public class User
+    {
+        public int UserId { get; set; }
 
-    public string Title { get; set; }
+        public string Name { get; set; }
 
-    public string Description { get; set; }
+        public string Email { get; set; }
 
-    public string Status { get; set; }
+        public string Role { get; set; }
+    }
 }
