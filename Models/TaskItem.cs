ee6aad629f27a2e97a18a237cb68a438c8f6621d
JyoshnaSreepuram <sreepuramjyoshna22@gmail.com>
2026-07-11 22:23:57 +0530

    Add TaskItem model with properties

---
+++ Models/TaskItem.cs
@@ -0,0 +1,17 @@
+namespace SmartTaskManagerAPI.Models
+{
+    public class TaskItem
+    {
+        public int TaskId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        public string Priority { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
